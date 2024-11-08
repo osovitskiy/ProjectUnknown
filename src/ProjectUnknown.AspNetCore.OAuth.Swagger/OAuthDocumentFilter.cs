427cf5949500cd737af6d303a84a611d6c0e015f
Oleg Osovitskiy <oleg.osovitskiy@outlook.com>
2024-11-08 00:30:58 +0000

    Add Swagger helper for OAuth module

---
+++ src/ProjectUnknown.AspNetCore.OAuth.Swagger/OAuthDocumentFilter.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace ProjectUnknown.AspNetCore.OAuth.Swagger
+{
+    public class OAuthDocumentFilter : IDocumentFilter
+    {
+        public OAuthDocumentFilter(PathString tokenEndpoint)
+        {
+            TokenEndpoint = tokenEndpoint;
+        }
+
+        public PathString TokenEndpoint { get; set; }
+
+        public void Apply(SwaggerDocument swaggerDoc, DocumentFilterContext context)
+        {
+            swaggerDoc.Paths = GetOAuthPaths().Concat(swaggerDoc.Paths).ToDictionary(x => x.Key, x => x.Value);
+        }
+
+        private IDictionary<string, PathItem> GetOAuthPaths() => new Dictionary<string, PathItem>()
+        {
+            {
+                TokenEndpoint, new PathItem()
+                {
+                    Post = new Operation()
+                    {
+                        Tags = new List<string>() {"OAuth"},
+                        Consumes = new List<string>() {"application/x-www-urlformencoded"},
+                        Produces = new List<string>() {"application/json"},
+                        Parameters = new List<IParameter>()
+                        {
+                            new NonBodyParameter()
+                            {
+                                Name = "grant_type",
+                                In = "formData",
+                                Type = "string",
+                                Enum = new List<object>()
+                                {
+                                    "password",
+                                    "refresh_token"
+                                },
+                                Required = true
+                            },
+                            new NonBodyParameter()
+                            {
+                                Name = "username",
+                                In = "formData",
+                                Type = "string"
+                            },
+                            new NonBodyParameter()
+                            {
+                                Name = "password",
+                                In = "formData",
+                                Type = "string"
+                            },
+                            new NonBodyParameter()
+                            {
+                                Name = "refresh_token",
+                                In = "formData",
+                                Type = "string"
+                            }
+                        },
+                        Responses = new Dictionary<string, Response>()
+                        {
+                            {
+                                "200", new Response()
+                                {
+                                    Description = "Success",
+                                    Schema = new Schema()
+                                    {
+                                        Type = "object",
+                                        Properties = new Dictionary<string, Schema>()
+                                        {
+                                            {
+                                                "access_token", new Schema()
+                                                {
+                                                    Type = "string"
+                                                }
+                                            },
+                                            {
+                                                "token_type", new Schema()
+                                                {
+                                                    Type = "string"
+                                                }
+                                            },
+                                            {
+                                                "expires_in", new Schema()
+                                                {
+                                                    Type = "integer"
+                                                }
+                                            },
+                                            {
+                                                "expires_at", new Schema()
+                                                {
+                                                    Type = "string"
+                                                }
+                                            },
+                                            {
+                                                "refresh_token", new Schema()
+                                                {
+                                                    Type = "string"
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
+    }
+}
---
+++ src/ProjectUnknown.AspNetCore.OAuth.Swagger/OAuthOperationFilter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace ProjectUnknown.AspNetCore.OAuth.Swagger
+{
+    public class OAuthOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null)
+            {
+                operation.Parameters = new List<IParameter>();
+            }
+
+            operation.Parameters.Insert(0, new NonBodyParameter()
+            {
+                Name = "Authorization",
+                In = "header",
+                Type = "string",
+                Description = "Access Token",
+                Required = false
+            });
+        }
+    }
+}
