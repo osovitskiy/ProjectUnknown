c9b44ca64a9ebd6f74d8f35d8970a4906bc262a5
Oleg Osovitskiy <oleg.osovitskiy@outlook.com>
2024-11-08 11:04:54 +0000

    Add Nuget push script

---
+++ nuget-push.cmd
@@ -0,0 +1,9 @@
+dotnet build -c Release
+dotnet nuget push %* src/ProjectUnknown.AspNetCore.Authentication.BasicAuthentication/bin/Release/ProjectUnknown.AspNetCore.Authentication.BasicAuthentication.3.0.0.nupkg
+dotnet nuget push %* src/ProjectUnknown.AspNetCore.DataProtection.MongoDB/bin/Release/ProjectUnknown.AspNetCore.DataProtection.MongoDB.3.0.0.nupkg
+dotnet nuget push %* src/ProjectUnknown.AspNetCore.Extensions/bin/Release/ProjectUnknown.AspNetCore.Extensions.3.0.0.nupkg
+dotnet nuget push %* src/ProjectUnknown.AspNetCore.Mvc.Formatters.MultipartRelated/bin/Release/ProjectUnknown.AspNetCore.Mvc.Formatters.MultipartRelated.3.0.0.nupkg
+dotnet nuget push %* src/ProjectUnknown.AspNetCore.OAuth/bin/Release/ProjectUnknown.AspNetCore.OAuth.3.0.0.nupkg
+dotnet nuget push %* src/ProjectUnknown.AspNetCore.OAuth.Swagger/bin/Release/ProjectUnknown.AspNetCore.OAuth.Swagger.1.0.0.nupkg
+dotnet nuget push %* src/ProjectUnknown.BaseEncoding/bin/Release/ProjectUnknown.BaseEncoding.1.0.0.nupkg
+dotnet nuget push %* src/ProjectUnknown.Localization.NGettext/bin/Release/ProjectUnknown.Localization.NGettext.3.0.0.nupkg
